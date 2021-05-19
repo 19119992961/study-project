95a180eb5febf9a8c2b2ac12e13c9928189a6521
张涛 <76242714+19119992961@users.noreply.github.com>
2021-05-19 17:36:42 +0800

    Update SchedulingConfig.java

--- src/main/java/com/zt/config/SchedulingConfig.java
+++ src/main/java/com/zt/config/SchedulingConfig.java
@@ -21,7 +21,7 @@ public class SchedulingConfig {
     @Async
     @Scheduled(fixedDelay = 1000)  //间隔1秒
     public void first() throws InterruptedException {
-        System.out.println("第一个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+        System.out.println("定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
         Thread.sleep(1000 * 10);
     }
 }
