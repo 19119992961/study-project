f505ff62db5eb23c42bef3e986248af3ec4ee01b
zt <zt1564404102@163.com>
2021-05-20 09:55:13 +0800

    second commit

--- src/main/java/com/zt/config/SchedulingConfig.java
+++ src/main/java/com/zt/config/SchedulingConfig.java
@@ -1,5 +1,6 @@
 package com.zt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -17,10 +18,13 @@ import java.time.LocalDateTime;
 @EnableScheduling
 @EnableAsync
 public class SchedulingConfig {
+    @Value("${url.crawlingUrl}")
+    private String crawlingUrl;
 
     @Async
     @Scheduled(fixedDelay = 1000)  //间隔1秒
     public void first() throws InterruptedException {
+        System.out.println(crawlingUrl);
         System.out.println("定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
         Thread.sleep(1000 * 10);
     }
